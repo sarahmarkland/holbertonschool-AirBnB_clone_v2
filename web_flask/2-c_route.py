a0f3da555d04fdc345edc95baec07e5a3a077215
Christopher <christopher@Christophers-MacBook-Pro.local>
2023-07-17 11:50:51 -0500

    adding blank lines

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,18 +7,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def textC(text):
     new_text = text.replace("_", " ")
     return ('C ' + new_text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
