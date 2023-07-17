9959303df1f5193adfec802d550a25e3bee9e42d
Christopher <christopher@Christophers-MacBook-Pro.local>
2023-07-17 12:28:53 -0500

    4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def textC(text):
+    new_text = text.replace("_", " ")
+    return 'C ' + new_text
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def textPython(text='is cool'):
+    return 'Python ' + text.replace("_", " ")
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def textPython(n):
+    if n is int:
+        return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
