46e068386e1f95df2f13f06420730d104a0ad02e
Christopher <christopher@Christophers-MacBook-Pro.local>
2023-07-17 13:22:56 -0500

    task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+/python/<text>: display “Python ”, followed by the value of the text
+    variable (replace underscore _ symbols with a space )
+The default value of text is “is cool”
+/number/<n>: display “n is a number” only if n is an integer
+You must use the option strict_slashes=False in your route definition
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def textC(text):
+    new_text = text.replace("_", " ")
+    return ('C ' + new_text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def textPython(text='is cool'):
+    return ('Python ' + text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return ("{} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even_template(n):
+    if n % 2 == 0:
+        text = 'even'
+    else:
+        text = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, text=text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
